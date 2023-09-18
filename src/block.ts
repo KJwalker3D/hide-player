ee245da04ad14214523549637904c77b285ed4dd
KatherineJoelle <53322457+KatherineJoelle@users.noreply.github.com>
2023-09-19 00:49:57 +0200

    first commit

---
+++ src/block.ts
@@ -0,0 +1,118 @@
+import { getPlayersInScene, PlayersGetUserDataResponse, getConnectedPlayers, getPlayerData } from '~system/Players'
+import { executeTask } from '@dcl/sdk/ecs'
+import { onEnterSceneObservable, onLeaveSceneObservable} from '@dcl/sdk/observables'
+import { onPlayerClickedObservable} from '@dcl/sdk/observables'
+import { customPrompt } from './ui'
+
+/// Simplest solution is to
+/// Hide avatar by clicking on it
+
+
+onPlayerClickedObservable.add((clickEvent) => {
+  console.log("Clicked ", clickEvent.userId, " details: ", clickEvent.ray);
+  // Display a pop-up "Hide this player? Yes / No"
+  customPrompt.show()
+})
+
+
+
+
+
+
+
+/// V2
+/*
+
+export interface Player {
+    userId: string;
+    isVisible: boolean;
+    displayName: string;
+  }
+  
+  interface PlayerDataResponse {
+    name: string;
+    // Add other properties as needed to match the actual response
+  }
+  
+  export let playerList: Player[] = [];
+  
+
+  
+
+  executeTask(async () => {
+    let connectedPlayers = await getPlayersInScene({});
+    const playerPromises = connectedPlayers.players.map(async (player) => {
+      const userData: PlayersGetUserDataResponse = await getPlayerData({ userId: player.userId });
+      return {
+        userId: player.userId,
+        isVisible: true,
+        displayName: userData.data?.displayName || 'Unknown', // Use the actual property
+      };
+    });
+  
+    playerList = await Promise.all(playerPromises);
+  
+    // Log players and their display names
+    console.log('List of nearby players:', playerList);
+  });
+  
+  // Event when player enters scene
+  onEnterSceneObservable.add((player) => {
+    console.log('player entered scene: ', player.userId);
+    // You can add the player to the playerList here if needed.
+  });
+  
+  // Event when player leaves scene
+  onLeaveSceneObservable.add((player) => {
+    console.log('player left scene: ', player.userId);
+    // You can remove the player from the playerList here if needed.
+  });
+
+*/
+
+
+
+
+
+/// V1
+/*
+interface Player {
+    userId: string;
+    isVisible: boolean;
+}
+
+export let playerList: Player[] = [];
+
+// Get all players already in scene
+executeTask(async () => {
+  let connectedPlayers = await getPlayersInScene({})
+  connectedPlayers.players.forEach((player) => {
+    console.log('player is nearby: ', player.displayName);
+    playerList = connectedPlayers.players.map((player) => ({
+        userId: player.userId,
+        isVisible: true, //starts true by default
+    }));
+    
+    // Log players
+    console.log('List of nearby players:', playerList);
+  });
+  // Toggle player visibility 
+  
+  function togglePlayerVisibility(userId: string) {
+      const player = playerList.find((p) => p.userId === userId);
+      if (player) {
+          player.isVisible = !player.isVisible
+      }
+  } 
+})
+
+// Event when player enters scene
+onEnterSceneObservable.add((player) => {
+  console.log('player entered scene: ', player.userId)
+})
+
+// Event when player leaves scene
+onLeaveSceneObservable.add((player) => {
+  console.log('player left scene: ', player.userId)
+})
+*/
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { engine } from '@dcl/sdk/ecs'
+
+import { bounceScalingSystem, circularSystem } from './systems'
+//import { setupUi } from './ui'
+import { customPrompt } from './ui'
+
+// Defining behavior. See `src/systems.ts` file.
+engine.addSystem(circularSystem)
+engine.addSystem(bounceScalingSystem)
+
+export function main() {
+  // draw UI
+ // setupUi()
+ customPrompt.show()
+
+}
